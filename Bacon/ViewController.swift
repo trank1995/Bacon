6c41d768e25c527c7ce6211cd126931de0276edb
spoderman <spoderman@itsfast50210.its.carleton.edu>
2016-01-04 15:28:46 -0600

    Initial Commit

---
+++ Bacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bacon
+//
+//  Created by spoderman on 1/4/16.
+//  Copyright © 2016 spoderman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
